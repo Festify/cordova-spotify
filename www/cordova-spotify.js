b380513e17e36f3e19fbc5dddeab43063349586b
mraerino <melodie124@gmail.com>
2016-12-03 02:08:16 +0100

    :bug: Fix internal module not working in cordova

--- www/cordova-spotify.js
+++ www/cordova-spotify.js
@@ -1,4 +1,20 @@
-var exec = require('./exec');
+var _exec = require('cordova/exec');
+
+var exec = function(className, methodName, args, callback) {
+    if (!methodName || !className) {
+        throw new Error("Missing method or class name argument (1st).");
+    }
+
+    _exec(function (res) {
+        if (callback) {
+            callback(null, res);
+        }
+    }, function (err) {
+        if (callback) {
+            callback(err);
+        }
+    }, className, methodName, args);
+};
 
 function Session(sessionObject) {
     if (!(this instanceof Session)) {
