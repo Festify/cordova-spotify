0d6628f270c5a8de44a08f3e469eb060b93f9056
Moritz Gunz <moritz.gunz@gmail.com>
2016-12-04 13:43:05 +0100

    :art: Indent CordovaSpotify interface declaration

--- src/ios/CordovaSpotify.h
+++ src/ios/CordovaSpotify.h
@@ -7,18 +7,17 @@
 #import <SafariServices/SafariServices.h>
 
 @interface CordovaSpotify : CDVPlugin <SPTAudioStreamingDelegate>
+    @property (nonatomic) bool isLoggedIn;
+    @property (nonatomic, strong) SPTSession* session;
+    @property (nonatomic, strong) SPTAudioStreamingController* player;
 
-@property (nonatomic) bool isLoggedIn;
-@property (nonatomic, strong) SPTSession* session;
-@property (nonatomic, strong) SPTAudioStreamingController* player;
+    - (void) pluginInitialize;
 
-- (void) pluginInitialize;
+    - (void) authenticate:(CDVInvokedUrlCommand*)command;
+    - (void) play:(CDVInvokedUrlCommand*)command;
+    - (void) setVolume:(CDVInvokedUrlCommand*)command;
 
-- (void) authenticate:(CDVInvokedUrlCommand*)command;
-- (void) play:(CDVInvokedUrlCommand*)command;
-- (void) setVolume:(CDVInvokedUrlCommand*)command;
-
-- (void) sendResultForCommand:(CDVInvokedUrlCommand*)cmd withError:(NSError*)err andSuccess:(NSString*)success;
+    - (void) sendResultForCommand:(CDVInvokedUrlCommand*)cmd withError:(NSError*)err andSuccess:(NSString*)success;
 @end
 
 #endif /* CordovaSpotify_h */
