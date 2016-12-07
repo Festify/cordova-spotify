1d67d396c702fc2fb2a6d84479eecc29c87f39fc
Moritz Gunz <moritz.gunz@gmail.com>
2016-12-07 16:22:03 +0100

    :art: Silence curl download output

--- install-ios.sh
+++ install-ios.sh
@@ -4,4 +4,4 @@ INSTALL_PATH="plugins/rocks.festify.cordova-spotify/src/ios/spotify-sdk"
 DOWNLOAD_PATH="https://github.com/spotify/ios-sdk/archive/beta-25.tar.gz"
 
 mkdir -p $INSTALL_PATH
-curl -L $DOWNLOAD_PATH | tar -xzv -C $INSTALL_PATH --strip 1
+curl -LsS $DOWNLOAD_PATH | tar -xzv -C $INSTALL_PATH --strip 1
