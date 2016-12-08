7f939eba2255c7fe2ab758b4a1af3424d0c2da27
Moritz Gunz <moritz.gunz@gmail.com>
2016-12-08 12:33:15 +0100

    :art: Silence tar output in installer script

--- install-ios.sh
+++ install-ios.sh
@@ -4,4 +4,5 @@ INSTALL_PATH="plugins/rocks.festify.cordova-spotify/src/ios/spotify-sdk"
 DOWNLOAD_PATH="https://github.com/spotify/ios-sdk/archive/beta-25.tar.gz"
 
 mkdir -p $INSTALL_PATH
-curl -LsS $DOWNLOAD_PATH | tar -xzv -C $INSTALL_PATH --strip 1
+curl -LsS $DOWNLOAD_PATH | tar -xz -C $INSTALL_PATH --strip 1
+echo "iOS install successful."
