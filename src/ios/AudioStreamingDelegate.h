31aaedd9a4c25de94c623c5ee929fdba5008f9c1
mraerino <melodie124@gmail.com>
2016-12-27 17:47:21 +0100

    :sparkles: :apple: Implement playback error emission

--- src/ios/AudioStreamingDelegate.h
+++ src/ios/AudioStreamingDelegate.h
@@ -2,6 +2,7 @@
 #define AudioStreamingDelegate_h
 
 #import <SpotifyAudioPlayback/SPTAudioStreamingController.h>
+#import <SpotifyAudioPlayback/SPTAudioStreamingController_ErrorCodes.h>
 #import "CordovaEventEmitter.h"
 
 @interface AudioStreamingDelegate : CordovaEventEmitter <SPTAudioStreamingDelegate>
--- src/ios/AudioStreamingDelegate.m
+++ src/ios/AudioStreamingDelegate.m
@@ -1,5 +1,34 @@
 #import "AudioStreamingDelegate.h"
 
+NSString* getErrorString(NSError *error) {
+    NSDictionary *codeMatrix = @{
+        [NSNumber numberWithInteger: SPTErrorCodeNoError]: @"NoError",
+        [NSNumber numberWithInteger: SPTErrorCodeFailed]: @"Failed",
+        [NSNumber numberWithInteger: SPTErrorCodeInitFailed]: @"InitFailed",
+        [NSNumber numberWithInteger: SPTErrorCodeWrongAPIVersion]: @"WrongAPIVersion",
+        [NSNumber numberWithInteger: SPTErrorCodeNullArgument]: @"NullArgument",
+        [NSNumber numberWithInteger: SPTErrorCodeInvalidArgument]: @"InvalidArgument",
+        [NSNumber numberWithInteger: SPTErrorCodeUninitialized]: @"Uninitialized",
+        [NSNumber numberWithInteger: SPTErrorCodeAlreadyInitialized]: @"AlreadyInitialized",
+        [NSNumber numberWithInteger: SPTErrorCodeBadCredentials]: @"BadCredentials",
+        [NSNumber numberWithInteger: SPTErrorCodeNeedsPremium]: @"NeedsPremium",
+        [NSNumber numberWithInteger: SPTErrorCodeTravelRestriction]: @"TravelRestriction",
+        [NSNumber numberWithInteger: SPTErrorCodeApplicationBanned]: @"ApplicationBanned",
+        [NSNumber numberWithInteger: SPTErrorCodeGeneralLoginError]: @"GeneralLoginError",
+        [NSNumber numberWithInteger: SPTErrorCodeUnsupported]: @"Unsupported",
+        [NSNumber numberWithInteger: SPTErrorCodeNotActiveDevice]: @"NotActiveDevice",
+        [NSNumber numberWithInteger: SPTErrorCodeGeneralPlaybackError]: @"GeneralPlaybackError",
+        [NSNumber numberWithInteger: SPTErrorCodePlaybackRateLimited]: @"PlaybackRateLimited",
+    };
+
+    NSString *errorString = [codeMatrix objectForKey: [NSNumber numberWithInteger: [error code]]];
+
+    if(!errorString) {
+        return @"Unknown";
+    }
+    return errorString;
+}
+
 @implementation AudioStreamingDelegate
 
 - (void)audioStreamingDidDisconnect:(SPTAudioStreamingController *)audioStreaming {
@@ -12,6 +41,10 @@
 
 - (void)audioStreaming:(SPTAudioStreamingController *)audioStreaming didReceiveMessage:(NSString *)message {
     [self emit:@"connectionmessage" withData:@[message]];
+}
+
+- (void)audioStreaming:(SPTAudioStreamingController *)audioStreaming didReceiveError:(NSError *)error {
+    [self emit:@"playbackerror" withData:@[getErrorString(error)]];
 }
 
 @end
