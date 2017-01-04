3ed4da519dcd93031f5273ac3fc263b20d6fbe8e
Moritz Gunz <moritz.gunz@gmail.com>
2017-01-04 12:26:09 +0100

    :art: Decrease indentation in android platform js

--- www/platforms/android.js
+++ www/platforms/android.js
@@ -14,40 +14,42 @@ function decode(msg) {
     };
 }
 
+function authenticate(options) {
+    return exec('authenticate', [
+        options.urlScheme,
+        options.clientId,
+        options.scopes
+    ])
+    .then(res => {
+        return fetch(options.tokenSwapUrl, {
+            body: qs.stringify({ code: res.code }),
+            method: 'POST'
+        }).then(decode("Token service did not return a successful response code."));
+    })
+    .then(authData => {
+        return fetch(conf.SPOTIFY_WEB_API + '/me', {
+            headers: {
+                "Authorization": "Bearer " + authData.access_token
+            }
+        })
+        .then(decode("Spotify API did not return a successful response code."))
+        .then(me => {
+            authData.canonicalUsername = me.id;
+            return authData;
+        });
+    })
+    .then(authData => {
+        authData.expirationDate = Date.now() + (authData.expires_in * 1000);
+        return exec("initSession", [
+            authData.access_token,
+            authData.refresh_token,
+            authData.canonicalUsername,
+            authData.expirationDate,
+            options.tokenRefreshUrl
+        ]).then(() => authData);
+    });
+}
+
 module.exports = {
-    authenticate: function (options) {
-        return exec('authenticate', [
-            options.urlScheme,
-            options.clientId,
-            options.scopes
-        ])
-            .then(res => {
-                return fetch(options.tokenSwapUrl, {
-                    body: qs.stringify({ code: res.code }),
-                    method: 'POST'
-                }).then(decode("Token service did not return a successful response code."));
-            })
-            .then(authData => {
-                return fetch(conf.SPOTIFY_WEB_API + '/me', {
-                    headers: {
-                        "Authorization": "Bearer " + authData.access_token
-                    }
-                })
-                .then(decode("Spotify API did not return a successful response code."))
-                .then(me => {
-                    authData.canonicalUsername = me.id;
-                    return authData;
-                });
-            })
-            .then(authData => {
-                authData.expirationDate = Date.now() + (authData.expires_in * 1000);
-                return exec("initSession", [
-                    authData.access_token,
-                    authData.refresh_token,
-                    authData.canonicalUsername,
-                    authData.expirationDate,
-                    options.tokenRefreshUrl
-                ]).then(() => authData);
-            });
-    }
+    authenticate: authenticate
 };
