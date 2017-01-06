100d21c3ae44b17fe00a9e6cddd2c5af393d41dc
Moritz Gunz <moritz.gunz@gmail.com>
2017-01-06 12:14:33 +0100

    :construction: Travis WIP

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,5 +5,5 @@ set -ev
 if [ "$APP_TARGET" == "android" ]; then
     cordova build android;
 else
-    xcodebuild CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO" -project platforms/ios/HelloCordova.xcodeproj -configuration debug -destination 'platform=iOS Simulator,name=iPhone SE,OS=10.2';
+    xcodebuild -xcconfig /Users/travis/build/Festify/cordova-spotify/HelloCordova/platforms/ios/cordova/build-debug.xcconfig -workspace HelloCordova.xcworkspace -scheme HelloCordova -configuration Debug -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone SE,OS=10.2' build CONFIGURATION_BUILD_DIR=/Users/travis/build/Festify/cordova-spotify/HelloCordova/platforms/ios/build/emulator CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO";
 fi
