69409078c0e3f69b53a25d7bbc1ab73fc84dc39d
Moritz Gunz <moritz.gunz@gmail.com>
2017-01-06 12:33:32 +0100

    :construction: Travis WIP

--- .travis-build.sh
+++ .travis-build.sh
@@ -6,5 +6,5 @@ if [ "$APP_TARGET" == "android" ]; then
     cordova build android;
 else
     # 63A0BA5F-3911-496F-BF2E-512830B4AD7F is iPhone SE on iOS 10.2
-    xcodebuild -xcconfig ./platforms/ios/cordova/build-debug.xcconfig -workspace ./platforms/ios/HelloCordova.xcworkspace -scheme HelloCordova -configuration Debug -sdk iphonesimulator -destination 'id=63A0BA5F-3911-496F-BF2E-512830B4AD7F' build CONFIGURATION_BUILD_DIR=./platforms/ios/build/emulator CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO";
+    xcodebuild -xcconfig ./platforms/ios/cordova/build-debug.xcconfig -workspace ./platforms/ios/HelloCordova.xcworkspace -scheme HelloCordova -configuration Debug -sdk iphonesimulator -destination 'id=63A0BA5F-3911-496F-BF2E-512830B4AD7F' build  CONFIGURATION_BUILD_DIR=./platforms/ios/build/emulator HEADER_SEARCH_PATHS="$(OBJROOT)/UninstalledProducts/$(PLATFORM_NAME)/include" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO";
 fi
