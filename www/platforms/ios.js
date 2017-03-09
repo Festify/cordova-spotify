e6c9008cd1fbc612eb6a809bc2b9f0a1a594507e
leolabs <admin@leolabs.org>
2017-03-09 23:09:43 +0100

    :construction: Implement iOS session reauth

--- www/platforms/ios.js
+++ www/platforms/ios.js
@@ -1,3 +1,4 @@
+const conf = require('../lib/const.js');
 const exec = require('../lib/execPromise.js');
 const Promise = require('es6-promise');
 
@@ -5,7 +6,7 @@ module.exports = {
     authenticate: function (options) {
         if (!options.urlScheme || !options.clientId || !options.scopes ||
             !options.tokenSwapUrl || !options.tokenRefreshUrl) {
-            return Promise.reject("Missing parameters");
+            return Promise.reject(conf.MISSING_PARAMETERS_ERROR);
         }
 
         return exec('authenticate', [
@@ -13,6 +14,16 @@ module.exports = {
             options.clientId,
             options.scopes,
             options.tokenSwapUrl,
+            options.tokenRefreshUrl
+        ]);
+    },
+    login: function (options) {
+        if (!options.clientId || !options.tokenRefreshUrl) {
+            return Promise.reject(conf.MISSING_PARAMETERS_ERROR);
+        }
+
+        return exec('login', [
+            options.clientId,
             options.tokenRefreshUrl
         ]);
     }
