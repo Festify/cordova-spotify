f9c966b509a741eb047206356d39ef0a64c28a38
Moritz Gunz <moritz.gunz@gmail.com>
2017-03-09 19:36:03 +0100

    :sparkles: Implement Android reauth

--- www/lib/const.js
+++ www/lib/const.js
@@ -1,3 +1,4 @@
 module.exports = {
+    REFRESH_TOKEN_LS_NAME: "cordovaSpotifyRefreshToken",
     SPOTIFY_WEB_API: "https://api.spotify.com/v1"
 };
--- www/platforms/ios.js
+++ www/platforms/ios.js
@@ -1,7 +1,12 @@
 const exec = require('../lib/execPromise.js');
 
 module.exports = {
-    authenticate: function(options) {
+    authenticate: function (options) {
+        if (!options.urlScheme || !options.clientId || !options.scopes ||
+            !options.tokenSwapUrl || !options.tokenRefreshUrl) {
+            return Promise.reject("Missing parameters");
+        }
+
         return exec('authenticate', [
             options.urlScheme,
             options.clientId,
