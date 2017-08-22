762ee3ce5d3dbb92b851b2b798badce6d484802d
Moritz Gunz <moritz.gunz@gmail.com>
2017-08-22 18:26:29 +0200

    :sparkles: Ensure only one native method is invoked at a time

--- www/lib/exec-promise.js
+++ www/lib/exec-promise.js
@@ -1,19 +1,27 @@
+import queue from 'async/queue';
+
 const _exec = cordova.exec;
+const q = queue(({ methodName, args }, cb) => {
+    // Delay the resolution and rejection callbacks because
+    // the Spotify SDKs do not like being reinvoked from inside
+    // of an event handler function.
+    _exec(
+        val => setTimeout(() => cb(null, val)),
+        err => setTimeout(() => cb(err)),
+        'SpotifyConnector',
+        methodName,
+        args || []
+    )
+});
 
 export default function(methodName, args) {
     if (!methodName) {
         throw new Error("Missing method or class name argument (1st).");
     }
 
-    // Delay the resolution and rejection callbacks because
-    // the Spotify SDKs do not like being reinvoked from inside
-    // of an event handler function.
-    return new Promise((res, rej) => _exec(
-        val => setTimeout(() => res(val)),
-        err => setTimeout(() => rej(err)),
-        'SpotifyConnector',
-        methodName,
-        args || []
+    return new Promise((res, rej) => q.push(
+        { methodName, args }, 
+        (err, val) => err ? rej(err) : res(val)
     ))
         .catch(({ type, msg }) => {
             const e = new Error(msg);
